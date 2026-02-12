09e6a0bacaeecefa3d8aaab24a6c3f40e96a2dca
purofle <purofle@gmail.com>
2026-02-13 01:03:54 +0800

    reformatted

--- chrome.nix
+++ chrome.nix
@@ -4,5 +4,4 @@
     google-chrome-dev
   ];
 
-
 }
--- dev.nix
+++ dev.nix
@@ -1,12 +1,21 @@
 { lib, pkgs, ... }:
 
 {
-  home.packages = with pkgs;[
+  home.packages = with pkgs; [
     gh
     lazygit
+    osc
   ];
 
- programs.git = {
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+          IdentityAgent ~/.1password/agent.sock
+    '';
+  };
+
+  programs.git = {
     enable = true;
     signing = {
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnOUTy30lbNXW15Zt35RpCEtaSubMgyl+zOHv4RnNGQ";
